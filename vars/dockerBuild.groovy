5b08a6556c128110691ae5bfb1fdf68ae8def03f
prince2998 <princegupta2971998@gmail.com>
2023-11-07 21:18:12 +0530

    update new file

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,6 @@
 def call(String dockerHubUsername, String imageName) {
     // Build the Docker image
     sh "docker build --build-arg REACT_APP_RAPID_API_KEY=80d71cb39fmsh1edfd2f8994ba4ep1517c5jsn3da7e7bb7e63 -t ${imageName} ."
-    sh "sudo chmod 777 /var/run/docker.sock"
      // Tag the Docker image
     sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
     // Push the Docker image
